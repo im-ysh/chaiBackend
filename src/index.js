788dfe310a852d66275d92d22b91fc11d95a1e25
im-ysh <akula2vaishnavi7@gmail.com>
2025-04-05 17:00:48 +0530

    changes made in src/ondex.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 // require(`dotenv`).config({path: './env'})
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-// import express from "express"
-
-
-
-import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 
